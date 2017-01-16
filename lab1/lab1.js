f052279334bf0e06d692ae900ffd165e733052c2
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-01-17 00:56:53 +0300

    Shape implementation

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,6 +1,23 @@
 ;!function () {
   'use strict';
 
-  var name = 'Awesome';
-  console.log('Hello, ' + name + '!');
+  function Shape(name) {
+    this.name = name;
+
+    this.getName = function () {
+      return this.name;
+    };
+
+    this.draw = function () {
+      alert(formatName());
+    };
+
+    var formatName = function () {
+      return this.name + " with bind ";
+    }.bind(this);
+  }
+
+  var shape = new Shape("Shape");
+  shape.draw();
+
 }();
