3e2bada83a27e85c990c57135bf0865d179ba1ff
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-01-17 14:31:30 +0300

    shape inheritance

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -20,4 +20,37 @@
       console.log('x: ' + this.x + ', y: ' + this.y + ', width: ' + this.width + ', height: ' + this.height);
     };
   }
+
+  function Rectangle(x, y, width, height) {
+    Shape.call(this, x, y, width, height);
+
+    this.getName = function () {
+      return "Rectangle";
+    };
+
+    var parentDraw = this.draw;
+    this.draw = function () {
+      parentDraw.call(this);
+    }
+  }
+
+  var rectangle = new Rectangle(50, 100, 150, 200);
+  rectangle.draw();
+
+  function Ellipse(x, y, width, height) {
+    Shape.call(this, x, y, width,  height);
+
+    this.getName = function () {
+      return "Ellipse";
+    };
+
+    var parentDraw = this.draw;
+    this.draw = function () {
+      parentDraw.call(this);
+    }
+  }
+
+  var ellipse = new Ellipse(50, 100, 150, 200);
+  ellipse.draw();
+
 }();
